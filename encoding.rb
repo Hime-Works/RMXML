35c4bcba7ff62cca46bb436d7af83fe9b8118c3b
mlin <mlin@avemacorp.com>
2014-09-30 12:10:29 -0400

    examples

--- encoding.rb
+++
@@ -1,4 +0,0 @@
-class Encoding
-  ASCII_8BIT = "ASCII-8BIT"
-  UTF_8 = "UTF-8"
-end
--- rmxml.rb
+++ rmxml.rb
@@ -1,5 +1,37 @@
 $LOAD_PATH << "System" unless $LOAD_PATH.include?("System")
 
-require 'rexml/lib/encoding'
+require 'encoding'
 require 'rexml/rexml'
 require "rexml/document"
+
+# Purely for example purposes
+
+def parse
+  f = File.new("demo.xml")
+  doc = REXML::Document.new(f)
+  p doc.to_s
+end
+
+def create
+  a = $data_actors[1]
+  doc = REXML::Document.new
+  
+  objs = [a]
+  objs.each do |obj|
+    class_name = obj.class.to_s.gsub("RPG::", "RPG--")
+    node = doc.add_element(class_name)
+    obj.instance_variables.each do |ivar|
+      attr_name = ivar.to_s.gsub("@", "")
+      dat = obj.instance_variable_get(ivar)
+      if dat.is_a?(Enumerable)
+        p dat
+      else
+        node.attributes[attr_name] = dat
+      end
+    end
+  end
+  p doc.to_s
+  File.open("demo.xml","w") do |data|
+    data<<doc
+  end
+end
