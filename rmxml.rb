f0c04641fc92853f2e18dc0b4874e86bbe46ddc8
mlin <mlin@avemacorp.com>
2014-10-01 14:56:47 -0400

    added Tone, Color, Table, Rect, Time, Struct, Complex, Rational

--- rmxml.rb
+++ rmxml.rb
@@ -8,18 +8,24 @@ module RMXML
   
   Tag_String = "s"
   Tag_Array = "a"
-  Tag_Fixnum = "i"
   Tag_Bignum = "j"
-  Tag_Float = "f"
-  Tag_Object = "o"
-  Tag_Hash = "h"
-  Tag_Symbol = "m"
+  Tag_Complex = "v"
   Tag_False = "n"
-  Tag_True = "y"
-  Tag_Nil = "z"
-  Tag_Ref = "p"
+  Tag_Fixnum = "i"  
+  Tag_Float = "f"  
+  Tag_Hash = "h"
+  Tag_Nil = "z"  
+  Tag_Object = "o"
   Tag_Range = "r"
+  Tag_Rational = "l"
+  Tag_Ref = "p"
+  Tag_Struct = "u"
+  Tag_Symbol = "m"  
+  Tag_Time = "t"
+  Tag_True = "y"      
   
+  
+  # List of classes that need to be handled differently  
   def self.save_data(obj, path)
     # create XML. Not sure how to add declaration
     xml = REXML::Document.new
@@ -57,7 +63,32 @@ module RMXML
       Tag_True => :add_true,
       Tag_False => :add_false,
       Tag_Range => :add_range,
-      Tag_Nil => :add_nil
+      Tag_Nil => :add_nil,
+      Tag_Complex => :add_complex,
+      Tag_Struct => :add_struct,
+      Tag_Rational => :add_rational,
+      Tag_Time => :add_time,
+      
+      Table => :add_table,
+      Tone  => :add_tone,
+      Color => :add_color,
+      Rect  => :add_rect  
+    }
+    
+    Type_Map = {
+      Array   => Tag_Array,
+      Hash => Tag_Hash,
+      Fixnum => Tag_Fixnum,
+      String => Tag_String,
+      Float => Tag_Float,
+      Symbol => Tag_Symbol,
+      TrueClass => Tag_True,
+      FalseClass => Tag_False,
+      Range => Tag_Range,
+      Struct => Tag_Struct,
+      Complex => Tag_Complex,
+      Rational => Tag_Rational,      
+      Time => Tag_Time,
     }
     
     def self.generate(node, obj)
@@ -119,6 +150,38 @@ module RMXML
       node.text = obj
     end
     
+    def self.add_rational(node, obj)
+      add_node(node, obj.numerator)
+      add_node(node, obj.denominator)
+    end
+    
+    def self.add_complex(node, obj)
+      add_node(node, obj.real)
+      add_node(node, obj.imag)
+    end
+    
+    def self.add_struct(node, obj)
+      node.attributes["c"] = obj.xml_class.name
+      node.attributes["id"] = @id
+      @refs[obj] = @id
+      @id += 1
+
+      obj.each_pair do |key, val|        
+        child = add_node(node, val)        
+        child.attributes["a"] = key
+      end
+    end
+    
+    def self.add_time(node, obj)
+      add_node(node, obj.year)
+      add_node(node, obj.month)
+      add_node(node, obj.day)
+      add_node(node, obj.hour)
+      add_node(node, obj.min)
+      add_node(node, obj.sec)
+      add_node(node, obj.utc_offset)
+    end
+    
     def self.add_range(node, obj)
       child = node.add_element("i")
       child.attributes["a"] = "begin"
@@ -142,36 +205,67 @@ module RMXML
       node.attributes["id"] = @id
       @refs[obj] = @id
       @id+= 1
-      obj.instance_variables.each do |ivar|
-        val = obj.instance_variable_get(ivar)
-        child = add_node(node, val)
-        child.attributes["a"] = ivar
+      
+      
+      method = Method_Map[obj.xml_class]
+      # Special objects
+      if method
+        self.send(method, node, obj)
+        
+      # Normal objects
+      else
+        obj.instance_variables.each do |ivar|
+          val = obj.instance_variable_get(ivar)
+          child = add_node(node, val)
+          child.attributes["a"] = ivar
+        end
       end
     end
     
+    def self.add_table(node, obj)
+      add_node(node, obj.xsize)
+      add_node(node, obj.ysize)
+      add_node(node, obj.zsize)
+      data = []
+      for z in 0...obj.zsize
+        for y in 0...obj.ysize
+          for x in 0...obj.xsize
+            data << obj[x,y,z]
+          end
+        end
+      end
+      add_node(node, data)
+    end
+    
+    def self.add_color(node, obj)
+      add_node(node, obj.red)
+      add_node(node, obj.green)
+      add_node(node, obj.blue)
+      add_node(node, obj.alpha)
+    end
+    
+    def self.add_tone(node, obj)
+      add_node(node, obj.red)
+      add_node(node, obj.green)
+      add_node(node, obj.blue)
+      add_node(node, obj.gray)
+    end
+    
+    def self.add_rect(node, obj)
+      add_node(node, obj.x)
+      add_node(node, obj.y)
+      add_node(node, obj.width)
+      add_node(node, obj.height)
+    end
+    
     def self.get_type(obj)
+      type = Type_Map[obj.xml_class]
       if obj.nil?
         Tag_Nil
       elsif @refs.include?(obj)
         Tag_Ref
-      elsif obj.is_a?(Array)
-        Tag_Array
-      elsif obj.is_a?(Hash)
-        Tag_Hash
-      elsif obj.is_a?(Fixnum)
-        Tag_Fixnum
-      elsif obj.is_a?(String)
-        Tag_String
-      elsif obj.is_a?(Float)
-        Tag_Float
-      elsif obj.is_a?(Symbol)
-        Tag_Symbol
-      elsif obj.is_a?(TrueClass)
-        Tag_True
-      elsif obj.is_a?(FalseClass)
-        Tag_False
-      elsif obj.is_a?(Range)
-        Tag_Range
+      elsif type
+        type
       else
         Tag_Object
       end
@@ -197,7 +291,18 @@ module RMXML
       Tag_False  => :load_false,
       Tag_Nil    => :load_nil,
       Tag_Ref    => :load_ref,
-      Tag_Range  => :load_range
+      Tag_Complex => :load_complex,
+      Tag_Time => :load_time,
+      Tag_Struct => :load_struct,
+      Tag_Range  => :load_range,
+      Tag_Rational => :load_rational
+    }
+    
+    Class_Map = {
+      "Rect"  => :load_rect,
+      "Table" => :load_table,
+      "Color" => :load_color,
+      "Tone"  => :load_tone
     }
     
     def self.parse(node)
@@ -224,11 +329,17 @@ module RMXML
       id = node.attributes["id"]
       @refs[id] = obj
       
-      # Build the object
-      node.elements.each do |elmt|
-        attr_name = elmt.attributes["a"]
-        data = load_node(elmt)
-        obj.instance_variable_set(attr_name, data)
+      # Build custom object
+      method = Class_Map[node.attributes["c"]]
+      if method
+        self.send(method, node, obj)
+      # Build regular object
+      else
+        node.elements.each do |elmt|
+          attr_name = elmt.attributes["a"]
+          data = load_node(elmt)
+          obj.instance_variable_set(attr_name, data)
+        end
       end
       return obj
     end
@@ -272,6 +383,22 @@ module RMXML
       return node.text.to_sym
     end
     
+    def self.load_struct(node)
+      obj = build_object(node)
+      node.elements.each do |elmt|
+        key = elmt.attributes["a"]
+        value = load_node(elmt)
+        obj[key] = value
+      end
+      return obj
+    end
+    
+    def self.load_complex(node)
+      real = load_node(node.elements[1])
+      imag = load_node(node.elements[2])
+      return Complex(real, imag)
+    end
+    
     def self.load_true(node)
       return true
     end
@@ -291,9 +418,62 @@ module RMXML
       return Range.new(start, fin, excl_end)      
     end
     
+    def self.load_rational(node)
+      numer = load_node(node.elements[1])
+      denom = load_node(node.elements[2])
+      return Rational(numer, denom)
+    end
+    
     def self.load_ref(node)
       id = node.text
       return @refs[id]
+    end
+    
+    def self.load_time(node)
+      year = load_node(node.elements[1])
+      month = load_node(node.elements[2])
+      day = load_node(node.elements[3])
+      hour = load_node(node.elements[4])
+      min = load_node(node.elements[5])
+      sec = load_node(node.elements[6])
+      utc_offset = load_node(node.elements[7])
+      return Time.new(year, month, day, hour, min, sec, utc_offset)
+    end
+    
+    def self.load_rect(node, obj)
+      obj.x = load_node(node.elements[1])
+      obj.y = load_node(node.elements[2])
+      obj.width = load_node(node.elements[3])
+      obj.height = load_node(node.elements[4])
+    end
+    
+    def self.load_tone(node, obj)
+      obj.red = load_node(node.elements[1])      
+      obj.green = load_node(node.elements[2])
+      obj.blue = load_node(node.elements[3])
+      obj.gray = load_node(node.elements[4])
+    end
+    
+    def self.load_color(node, obj)
+      obj.red = load_node(node.elements[1])      
+      obj.green = load_node(node.elements[2])
+      obj.blue = load_node(node.elements[3])
+      obj.alpha = load_node(node.elements[4])
+    end
+    
+    def self.load_table(node, obj)
+      xsize = load_node(node.elements[1])
+      ysize = load_node(node.elements[2])
+      zsize = load_node(node.elements[3])
+      data = load_node(node.elements[4])
+      obj.resize(xsize, ysize, zsize)
+      for z in 0...obj.zsize
+        for y in 0...obj.ysize
+          for x in 0...obj.xsize
+            obj[x,y,z] = data[z * zsize + y * ysize + x]
+          end
+        end
+      end
     end
   end
 end
